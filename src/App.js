8306665244720c512b6b1a48dfd22da2f6aa242b
Thanh Thủy <thuynguyenthanh2001@gmail.com>
2026-05-03 00:48:29 +0700

    delete unused value

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import logo from './logo.svg';
-// import './App.css';
-import ColorBox from './ColorBox'
 import React from 'react';
 import TodoFeature from './features/Todo';
 function App() {
-  const name = "Nguyen Thanh Thuy";
-  const age = 20;
-  const isFemale = true;
-  const student = {
-    name: "Thanh Thuy",
-    age: 25,
-  }
-  const colorList = ["red", "blue", "green"];
   return (
     <div className="App">
       <TodoFeature />
