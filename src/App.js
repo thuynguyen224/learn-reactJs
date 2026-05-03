121c5691922fdfe80ebfb2facdeb621fc1554124
Thanh Thủy <thuynguyenthanh2001@gmail.com>
2026-05-04 02:40:23 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import TodoFeature from './features/Todo';
+import { NavLink, Route, Routes, Navigate } from 'react-router-dom';
+import AlbumFeature from './features/Album';
+import ListPage from './features/Todo/pages/ListPage';
+import DetailPage from './features/Todo/pages/DetailPage';
+import NotFound from './components/NotFound';
+
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
+      Header
+      <p><NavLink to='/todos' activeClassName="active-menu">Todo</NavLink></p>
+      <p><NavLink to='/albums' activeClassName="active-menu">Album</NavLink></p>
+      <Routes>
+        <Route path="/todos" element={<ListPage />}>
+          <Route index element={<ListPage />} />
+          <Route path=":todoId" element={<DetailPage />} />
+        </Route>
+        <Route path='/albums' element={<AlbumFeature />} />
+        <Route path='/not-found' element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/not-found" replace />} />
+      </Routes>
+
+      Footer
     </div>
   );
 }
---
+++ src/features/Todo/pages/ListPage/index.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useMemo, useState } from "react";
+import TodoList from "../../components/TodoList";
+import { useLocation, useNavigate, useMatch } from "react-router-dom";
+import queryString from "query-string";
+function ListPage(props) {
+    const initTodoList = [
+        {
+            id: 1,
+            title: "Eat",
+            status: "new",
+        },
+        {
+            id: 2,
+            title: "Sleep",
+            status: "completed",
+        },
+        {
+            id: 3,
+            title: "Code",
+            status: "new",
+        },
+    ];
+    const location = useLocation();
+    const match = useMatch("/todos");
+    const navigate = useNavigate();
+    const [todoList, settodoList] = useState(initTodoList);
+    const [filterStatus, setFilterStatus] = useState(() => {
+        const params = queryString.parse(location.search);
+        return params.status || "all";
+    });
+    useEffect(() => {
+        const params = queryString.parse(location.search);
+        setFilterStatus(params.status || "all");
+    }, [location.search]);
+
+    const handleTodoClick = (todo, idx) => {
+        // clone current array to the new one
+        const newTodoList = [...todoList];
+        // toggle state
+        newTodoList[idx] = {
+            ...newTodoList[idx],
+            status: newTodoList[idx].status === "new" ? "completed" : "new",
+        };
+        // update todo list
+        settodoList(newTodoList);
+    };
+    const handleShowAllClick = () => {
+        const queryParams = { status: "all" };
+        navigate({
+            pathname: match.path,
+            search: queryString.stringify(queryParams),
+        })
+    };
+    const handleShowCompletedClick = () => {
+        const queryParams = { status: "completed" };
+        navigate({
+            pathname: match.path,
+            search: queryString.stringify(queryParams),
+        })
+    };
+    const handleShowNewClick = () => {
+        const queryParams = { status: "new" };
+        navigate({
+            pathname: match.path,
+            search: queryString.stringify(queryParams),
+        })
+    };
+
+    const renderedTodoList = useMemo(() => {
+        return todoList.filter(todo => filterStatus === "all" || filterStatus === todo.status);
+    }, [todoList, filterStatus]);
+
+    return (
+        <div>
+            <h1>Todo Feature</h1>
+            <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
+            <div>
+                <button onClick={handleShowAllClick}>Show All</button>
+                <button onClick={handleShowCompletedClick}>Show Completed</button>
+                <button onClick={handleShowNewClick}>Show New</button>
+            </div>
+        </div>
+    );
+}
+
+export default ListPage;
