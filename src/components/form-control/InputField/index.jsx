db2c6b0c9077afef4c437d7613aefb24ae48e750
Thủy Nguyễn <thuynguyenthanh2001@gmail.com>
2026-05-07 01:30:53 +0700

    validation with yup

--- src/components/form-control/InputField/index.jsx
+++ src/components/form-control/InputField/index.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { TextField } from '@mui/material';
-import { Controller } from 'react-hook-form';
+import React from "react";
+import PropTypes from "prop-types";
+import { TextField } from "@mui/material";
+import { Controller } from "react-hook-form";
 
 InputField.propTypes = {
-    form: PropTypes.object.isRequired,
-    name: PropTypes.string.isRequired,
-    label: PropTypes.string,
-    disable: PropTypes.bool,
+  form: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  disable: PropTypes.bool,
 };
 
 function InputField(props) {
-    const { form, name, label, disable } = props;
+  const { form, name, label, disable } = props;
 
-    return (
-        <Controller
-            name={name}
-            control={form.control}
-            render={({ field }) => (   
-                <TextField
-                    fullWidth
-                    label={label}   
-                    disabled={disable}
-                    {...field}
-                />
-            )}
+  return (
+    <Controller
+      name={name}
+      control={form.control}
+      render={({ field, fieldState }) => (
+        <TextField
+          fullWidth
+          label={label}
+          disabled={disable}
+          {...field}
+          error={!!fieldState.error}
+          helperText={fieldState.error?.message}
         />
-    );
+      )}
+    />
+  );
 }
 
 export default InputField;
--- src/features/Todo/components/TodoForm/index.jsx
+++ src/features/Todo/components/TodoForm/index.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import InputField from '../../../../components/form-control/InputField';
-import { useForm } from 'react-hook-form';
+import React from "react";
+import PropTypes from "prop-types";
+import InputField from "../../../../components/form-control/InputField";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 TodoForm.propTypes = {
-    onsubmit: PropTypes.func,
+  onsubmit: PropTypes.func,
 };
 
 function TodoForm(props) {
-    const form = useForm({
-        defaultValues: {
-            title: "",
-        }
-    });
-    const handleSubmit = (values) => {
-        console.log("Form submit: ", values);
-    };  
-    return (
-        <form onSubmit={form.handleSubmit(handleSubmit)}>
-            <InputField name="title" label="Todo" form={form} />
-        </form>
-    );
+  const schema = yup
+    .object({
+      title: yup
+        .string()
+        .required("please enter title")
+        .min(5, "title is too short"),
+    })
+    .required();
+  const form = useForm({
+    defaultValues: {
+      title: "",
+    },
+    resolver: yupResolver(schema),
+  });
+  const handleSubmit = (values) => {
+    console.log("Form submit: ", values);
+  };
+  return (
+    <form onSubmit={form.handleSubmit(handleSubmit)}>
+      <InputField name="title" label="Todo" form={form} />
+    </form>
+  );
 }
 
 export default TodoForm;
