5ad6ca037dcc3d095e6740207dd54ba683878f4d
Thủy Nguyễn <thuynguyenthanh2001@gmail.com>
2026-05-11 23:31:44 +0700

    add close button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,15 +7,24 @@ import { makeStyles } from "@mui/styles";
 import CodeIcon from "@mui/icons-material/Code";
 import { Link, NavLink } from "react-router-dom";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import { useState } from "react";
 import Register from "features/Auth/components/Register";
+import { IconButton } from "@mui/material";
+import { Close } from "@mui/icons-material";
+import { grey } from "@mui/material/colors";
 
 const useStyles = makeStyles({
   link: {
     color: "white",
     textDecoration: "none",
+  },
+  closeButton: {
+    position: "absolute !important",
+    top: 1,
+    right: 1,
+    color: grey[500],
+    zIndex: 1,
   },
 });
 
@@ -62,16 +71,16 @@ export default function Header() {
       <Dialog
         open={open}
         onClose={(event, reason) => {
-          if (reason === "backdropClick" || "escapeKeyDown") return;
+          if (reason === "backdropClick" || reason === "escapeKeyDown") return;
           handleClose();
         }}
       >
+        <IconButton className={classes.closeButton} onClick={handleClose}>
+          <Close />
+        </IconButton>
         <DialogContent>
           <Register closeDialog={handleClose} />
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-        </DialogActions>
       </Dialog>
     </Box>
   );
